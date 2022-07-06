4aa99b4699225f2be20d5961df67c58daf13ad6b
BinaryLeo <binaryleo@pop-os.localdomain>
2022-07-06 00:20:31 -0300

    chore: create a account

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
+const StatusCodes = require('http-status-codes');
 const express = require('express');
+const {v4 : uuidv4} = require('uuid'); // V4 random
 const app = express(); //a new instance of express
-app.listen(3333)
+app.use(express.json())
+const customers = [];
+app.post("/account",(request,response)=>{   
+  const {cpf,name}=  request.body;  // get the cpf and name from the request
+  const id = uuidv4();
+  customers.push( 
+    {
+     cpf,
+     name,
+     id,
+     statement:[] ,
+    });
+    return response.send(StatusCodes.StatusCodes.OK);
+}) 
+app.listen(3333) 
